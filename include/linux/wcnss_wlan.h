4d19980d1146318607ad92b86de43d684551421f
Anand N Sunkad <asunka@codeaurora.org>
2015-01-06 14:50:49 +0530

    wcnss: get MUX control before dumping IRIS register
    Sometimes dumping IRIS register along with PRONTO register
    results in crash.
    To mitigate issue get MUX control before dumping IRIS register.
    
    Change-Id: Ie1e19a254ec1ae43c2713c86c4d35a2d9968bcd6
    CRs-Fixed: 777663
    Signed-off-by: Anand N Sunkad <asunka@codeaurora.org>
    Signed-off-by: Jean Francis Dominique S. Mabalot <jmabalot@google.com>

--- include/linux/wcnss_wlan.h
+++ include/linux/wcnss_wlan.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2013,2015 The Linux Foundation. All rights reserved.
  * Copyright (C) 2015 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
